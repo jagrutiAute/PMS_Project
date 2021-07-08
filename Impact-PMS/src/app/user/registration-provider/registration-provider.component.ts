8d7e7b814db45de2b093305a9eac56b000e9d8ba
Naushad Ansari <naushadansari1678@gmail.com>
2021-07-08 10:45:51 +0530

    provider registration validation

--- Impact-PMS/src/app/user/registration-provider/registration-provider.component.ts
+++ Impact-PMS/src/app/user/registration-provider/registration-provider.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Route } from '@angular/compiler/src/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-registration-provider',
   templateUrl: './registration-provider.component.html',
   styleUrls: ['./registration-provider.component.css']
 })
-export class RegistrationProviderComponent implements OnInit {
+export class RegistrationProviderComponent {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  
+  ) 
+  
+  { }
+
+  signUpFormProvider = this.fb.group({
+    title: ['', Validators.required],
+    firstName: ['', 
+              [Validators.required,
+              Validators.minLength(2)
+              ]
+            ],
+    lastName: ['', 
+              [Validators.required,
+              Validators.minLength(2)
+              ]],
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}')
+
+      ]
+    ],
+    dob:['', Validators.required],
+    role: ['', Validators.required],
+    employeeid:['', Validators.required]
+   
+
+
+  });
+
+  get f() {
+    return this.signUpFormProvider.controls;
   }
+
+
+
 
 }
