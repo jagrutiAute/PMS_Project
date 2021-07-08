4d65726104fe98f8b6a4907f27db7432433f634b
gaurav030694 <abale.gaurav94@gmail.com>
2021-07-08 10:37:06 +0530

    validation on forgot-password page

--- Impact-PMS/src/app/user/forgot-password/forgot-password.component.ts
+++ Impact-PMS/src/app/user/forgot-password/forgot-password.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
   styleUrls: ['./forgot-password.component.css']
 })
-export class ForgotPasswordComponent implements OnInit {
+export class ForgotPasswordComponent  {
 
-  constructor() { }
+  user: User;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    
+  ) { }
+
+  forgotPassForm = this.fb.group({
+
+    email: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern('[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}')
+
+      ]
+    ]
+  });
+
+  get f() {
+    return this.forgotPassForm.controls;
+  }
+
+  forgotPass(){
+
+    //call service after that redirect to login page
   }
 
 }
