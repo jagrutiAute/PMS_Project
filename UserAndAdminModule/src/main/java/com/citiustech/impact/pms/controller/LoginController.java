59351c271b158083a493d2eb2d06f859d1e2d35b
shivraj123 <pawarshivraj379@gmail.com>
2021-07-31 17:02:23 +0530

    implment encryption functionality and resolve issue, added log4j message for login controller class

--- UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/LoginController.java
+++ UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/LoginController.java
@@ -1,5 +1,9 @@
 package com.citiustech.impact.pms.controller;
 
+import java.io.File;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import com.citiustech.impact.pms.DTO.LoginDTO;
 import com.citiustech.impact.pms.model.ISActive;
 import com.citiustech.impact.pms.model.Users;
 import com.citiustech.impact.pms.service.LoginService;
+import com.citiustech.impact.pms.service.PatientServiceImpl;
 import com.mysql.cj.log.LogFactory;
 
 /*
@@ -24,64 +29,76 @@ import com.mysql.cj.log.LogFactory;
 @CrossOrigin(origins = "*")
 public class LoginController {
 
+	private static Logger logger = Logger.getLogger(LoginController.class);
+
 	@Autowired
 	private LoginService userService;
 
 	@PostMapping("/login")
 	public ResponseEntity<String> check(@RequestBody LoginDTO log) {
+		PropertyConfigurator.configure(System.getProperty("user.dir") + File.separator + "log4j.properties");
 
-		System.out.println("++++++++ " + log);
-		Users loginResult = userService.login(log.getEmail());
+		logger.trace("Initializaing check() method ");
+		logger.trace("calling encryption() method to encrypt password");
 
-		System.out.println("loginResult  ::  " + loginResult);
+		String pwd = PatientServiceImpl.encryption(log.getEmail(), log.getPassword());
+
+		logger.debug(PatientServiceImpl.encryption(log.getEmail(), log.getPassword()));
+
+		logger.trace("calling login() method ");
+		Users loginResult = userService.login(log.getEmail(), pwd);
+		logger.info("login info " + loginResult);
 
 		if (loginResult == null) {
-			return new ResponseEntity<String>("UsernamePass",HttpStatus.OK);
+			logger.error("user doesn't exist in system");
+			return new ResponseEntity<String>("UsernamePass", HttpStatus.OK);
+
 		}
 
-			if (loginResult.getIsActive() == ISActive.BLOCK) {
+		if (loginResult.getIsActive() == ISActive.BLOCK) {
 
-				System.out.println("already blocked");
-				// return status your account has blocked contact Admin
+			logger.info("user status is blocked");
+			return new ResponseEntity<String>("BLOCKED", HttpStatus.OK);
+
+		}
+
+		logger.warn("user attements is :" +loginResult.getLoginAttempts() + " after providng 3 time wrong credentials user will enter into block state");
+
+		int n = loginResult.getLoginAttempts();
+
+		logger.trace("check the password is wrong");
+		if (!loginResult.getPassword().equals(pwd)) {
+		logger.debug("Wrong attempt count" + n);
+			if (n == 2) {
+				logger.info("3 wrong attempt , user status set as block");
+				loginResult.setIsActive(ISActive.BLOCK);
+				logger.trace("calling blockUser() method");
+				userService.blockUser(loginResult);
+				
+				logger.error("Account has blocked due to 3 time wrong attempts");
+			
 
 				return new ResponseEntity<String>("BLOCKED", HttpStatus.OK);
 
-			}
+			} else {
 
-			System.out.println(loginResult.getLoginAttempts());
+				logger.trace("set the login attempts count");
+				loginResult.setLoginAttempts(n + 1);
+				logger.trace("calling updatefaildeLogin method");
+				Users re = userService.updatefaildeLogin(loginResult);
+				logger.debug(userService.updatefaildeLogin(loginResult));
 
-			int n = loginResult.getLoginAttempts();
-			
-		
-
-			if (!loginResult.getPassword().equals(log.getPassword())) {
-
-				if (n == 2) {
-
-					loginResult.setIsActive(ISActive.BLOCK);
-
-					userService.blockUser(loginResult);
-					System.out.println(loginResult);
-
-					System.out.println("your account has blocked");
-
-					return new ResponseEntity<String>("BLOCKED", HttpStatus.OK);
-
-				} else {
-
-					loginResult.setLoginAttempts(n + 1);
-
-					Users re = userService.updatefaildeLogin(loginResult);
-
-					return new ResponseEntity<String>(String.valueOf(re.getLoginAttempts()), HttpStatus.OK);
-
-				}
+				return new ResponseEntity<String>(String.valueOf(re.getLoginAttempts()), HttpStatus.OK);
 
 			}
-			
-		 else {
+
+		}
+
+		else {
+			logger.trace("User validate successfully");
 			return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
 		}
+		
 
 	}
 
--- UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/ProviderRegistrationController.java
+++ UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/ProviderRegistrationController.java
@@ -1,6 +1,5 @@
 package com.citiustech.impact.pms.controller;
 
-import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.citiustech.impact.pms.DTO.ProviderRegistrationDTO;
-import com.citiustech.impact.pms.model.ProviderRegistration;
-import com.citiustech.impact.pms.model.Users;
-import com.citiustech.impact.pms.service.EmailService;
 import com.citiustech.impact.pms.service.ProviderRegistrationService;
-import com.citiustech.impact.pms.service.RoleService;
-import com.citiustech.impact.pms.service.UserRegistrationService;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -24,42 +18,14 @@ public class ProviderRegistrationController {
 	@Autowired
 	ProviderRegistrationService providerRegService;
 
-	@Autowired
-	RoleService role;
 
-	@Autowired
-	UserRegistrationService userRegService;
-
-	@Autowired
-	EmailService emailService;
 
 	@PostMapping("/registerProvider")
 	public ResponseEntity<String> registerProvider(@RequestBody ProviderRegistrationDTO regProvider) {
 
-		Users user = new Users();
-		user.setEmail(regProvider.getUsername());
-		user.setRole(role.getRoles().get(regProvider.getRole()));
-		String generatedString = RandomStringUtils.random(10, true, true).concat("$");
-		
-		/*
-		 * emailService.sendEmail("impactpmsjavabatch1@gmail.com", "amit@1234",
-		 * regProvider.getUsername(), "Reset Pasword OTP ", "Your one time password is "
-		 * + generatedString);
-		 */
-
-		user.setPassword(generatedString);
-
 	
-		ProviderRegistration provider = new ProviderRegistration();
-
-		provider.setTitle(regProvider.getTitle());
-		provider.setFirstName(regProvider.getFirstName());
-		provider.setLastName(regProvider.getLastName());
-		provider.setDate_of_birth(regProvider.getDate_of_birth());
-		provider.setEmployeeid(regProvider.getEmployeeid());
-		provider.setUser(user);
 		
-		String status = providerRegService.registerProvider(provider, regProvider.getUsername());
+		String status = providerRegService.registerProvider(regProvider, regProvider.getUsername());
 
 		return new ResponseEntity<String>(status,HttpStatus.OK);
 
