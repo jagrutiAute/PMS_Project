750b93efdcb9b4bb5724d4e68d9f596368cf305e
gaurav030694 <abale.gaurav94@gmail.com>
2021-08-10 09:29:27 +0530

    added new dashboard

---
+++ admin-panel-master/src/app/user/patient-registration.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Patient } from "./patient";
+import { User1 } from "./user1";
+
+
+@Injectable({ providedIn: 'root'})
+
+
+export class PatientService {
+ 
+  private baseUrl = 'http://localhost:8088/register'; 
+  
+  constructor(private _http: HttpClient) { }
+
+   
+
+  getPatientRegister(patient: Patient): Observable<any> {
+   
+
+      //return this._http.post(`${this.baseUrl}`, patient);
+      return this._http.post(`${this.baseUrl}`, patient,{responseType: 'text' as 'json'}); 
+      
+  }
+}
---
+++ admin-panel-master/src/app/user/user.routes.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from '@angular/core'
+import { Route, RouterModule } from '@angular/router'
+import { AdminDashboardComponent } from '../admin/admin-dashboard/admin-dashboard.component'
+
+import { ChangePasswordComponent } from './change-password/change-password.component'
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
+import { LoginComponent } from './login/login.component'
+import { RegistrationPatientComponent } from './registration-patient/registration-patient.component'
+import { RegistrationProviderComponent } from './registration-provider/registration-provider.component'
+
+
+export var userroutes:Route[]=[
+    {path:'login', component:LoginComponent},
+    
+    {path:'forgot-password', component:ForgotPasswordComponent},
+    {path:'patient-registration', component:RegistrationPatientComponent},
+    {path:'app-registration-provider', component:RegistrationProviderComponent},
+    {path:'admin-dashboard', component:AdminDashboardComponent}
+]
+
