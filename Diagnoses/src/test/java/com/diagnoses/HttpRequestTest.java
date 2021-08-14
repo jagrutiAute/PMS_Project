53f88480c6e341f44f1035d233f0c3f36a82bd98
gajun755 <gajun755@gmail.com>
2021-08-14 12:40:50 +0530

    added confi server,api-gateway

---
+++ Diagnoses/src/test/java/com/diagnoses/HttpRequestTest.java
@@ -0,0 +1,32 @@
+package com.diagnoses;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import com.diagnoses.controller.AppInfoController;
+
+@SpringBootTest(classes = AppInfoController.class)
+@RunWith(SpringRunner.class)
+public class HttpRequestTest {
+		
+	protected MockMvc mvc;
+	
+	@Test
+	public void testapiinfocorrect() {
+	
+	}
+	
+	
+		
+}
