4a4caa5aaff72ea0afdb273bb83929072d74b49d
shivraj123 <pawarshivraj379@gmail.com>
2021-08-14 17:56:00 +0530

    Report Added

--- Observations/src/main/java/com/citiustech/impact/pms/procedure/controller/DemoController.java
+++ Observations/src/main/java/com/citiustech/impact/pms/procedure/controller/DemoController.java
@@ -1,4 +1,4 @@
-package com.observations.controller;
+package com.citiustech.impact.pms.procedure.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
