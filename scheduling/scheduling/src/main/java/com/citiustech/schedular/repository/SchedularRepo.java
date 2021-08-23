1345ab57343b24a00cbd00896f80e2bb6cd9759e
gajun755 <gajun755@gmail.com>
2021-08-23 13:27:57 +0530

    completed end to end flow of cancel appointment

--- scheduling/scheduling/src/main/java/com/citiustech/schedular/repository/SchedularRepo.java
+++ scheduling/scheduling/src/main/java/com/citiustech/schedular/repository/SchedularRepo.java
@@ -4,12 +4,18 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.citiustech.schedular.model.Schedular;
 
 public interface SchedularRepo extends JpaRepository<Schedular, Integer> {
 		
 		Schedular findByPhyidAndDateAndTime(String phy_id,LocalDate date,String time);
-		List<Schedular> findByPhyidAndDateAndBooked(String phid,LocalDate date,boolean book_status);
+		List<Schedular> findByPhyidAndDateAndBookedAndIscancelled(String phid,LocalDate date,boolean book_status,boolean isCancelled );
+		//List<Schedular> findByPhyidAndDateAndBooked(String phid,LocalDate date,boolean book_status);
+		@Query("select a from Schedular a where a.date >=:date and phyid=:pid and isCancelled=:st ")
+		List<Schedular> findAllWithDateAfter(LocalDate date,String pid,boolean st);
 		
+		Schedular findByPhyidAndDateAndPidAndTime(String phyid,LocalDate date,String pid,String time);
+		Schedular findByPidAndTimeAndDate(String pid,String time,LocalDate date);
 }
