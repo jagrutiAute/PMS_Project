d0fdfd225b55b6a0af90af84f4f0c22f0c427c06
farheenbaig108 <farheenbaig108@gmail.com>
2021-09-01 21:39:13 +0530

    Added PDF code for medication

--- UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/MedicationController.java
+++ UserAndAdminModule/src/main/java/com/citiustech/impact/pms/controller/MedicationController.java
@@ -1,12 +1,18 @@
 package com.citiustech.impact.pms.controller;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +26,9 @@ import com.citiustech.impact.pms.model.Medication;
 import com.citiustech.impact.pms.model.MedicationSave;
 //import com.citiustech.impact.pms.model.MedicationSave;
 import com.citiustech.impact.pms.repository.MedicationRepo;
+import com.citiustech.impact.pms.repository.MedicationSaveRepository;
 import com.citiustech.impact.pms.service.MedicationService;
+import com.citiustech.impact.pms.util.PDFGenerator;
 //import com.citiustech.impact.pms.repository.MedicationSaveRepository;
 //import com.citiustech.impact.pms.service.MedicationService;
 
@@ -32,60 +40,80 @@ public class MedicationController {
 
 	@Autowired
 	private MedicationRepo medicationRepo;
-	
+
 	@Autowired
 	private MedicationService medicationService;
-	
+
+	@Autowired
+	private MedicationSaveRepository medicationSaveRepo;
+
 	@GetMapping("physician/medication")
-	public ResponseEntity<List<Medication>> getMedicationDetails(){
+	public ResponseEntity<List<Medication>> getMedicationDetails() {
 		System.out.println("inside getMedicationDetails=");
 		List<Medication> medication = medicationRepo.findAllMedication();
-		
+
 		log.debug("inside getMedicationDetails() method");
 		log.debug("calling findAll() method of MedicationRepo interface");
-		
-		//List<Medication> medication = medicationRepo.findAll();
+
+		// List<Medication> medication = medicationRepo.findAll();
 		return new ResponseEntity<List<Medication>>(medication, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("physician/addmedication/{pid}/{phyid}")
-	public String addMedicationForPatient(@RequestBody List<MedicationSave> medicationObject, @PathVariable String pid, @PathVariable String phyid){
-		
-		System.out.println("pid="+pid);
-		System.out.println("phyid="+phyid);
+	public String addMedicationForPatient(@RequestBody List<MedicationSave> medicationObject, @PathVariable String pid,
+			@PathVariable String phyid) {
+
+		System.out.println("pid=" + pid);
+		System.out.println("phyid=" + phyid);
 		List<MedicationSave> medicationSaves = new ArrayList<>();
-		for(MedicationSave m: medicationObject) {
-			
-		    MedicationSave m1 = new MedicationSave();
-		    m1.setApplNo(m.getApplNo());
-		    m1.setProductNo(m.getProductNo());
-		    m1.setForm(m.getForm());
-		    m1.setStrength(m.getStrength());
-		    m1.setReferenceDrug(m.getReferenceDrug());
-		    m1.setDrugName(m.getDrugName());
-		    m1.setActiveIngredient(m.getActiveIngredient());
-		    m1.setReferenceStandard(m.getReferenceStandard());
-		    m1.setPid(pid);
-		    m1.setPhyid(phyid);
-		    m1.setDate(LocalDate.now());
-		    medicationSaves.add(m1);
+		for (MedicationSave m : medicationObject) {
+
+			MedicationSave m1 = new MedicationSave();
+			m1.setApplNo(m.getApplNo());
+			m1.setProductNo(m.getProductNo());
+			m1.setForm(m.getForm());
+			m1.setStrength(m.getStrength());
+			m1.setReferenceDrug(m.getReferenceDrug());
+			m1.setDrugName(m.getDrugName());
+			m1.setActiveIngredient(m.getActiveIngredient());
+			m1.setReferenceStandard(m.getReferenceStandard());
+			m1.setPid(pid);
+			m1.setPhyid(phyid);
+			m1.setDate(LocalDate.now());
+			medicationSaves.add(m1);
 		}
-		medicationSaves.stream().forEach(x->System.out.println(x));
-		
-		//medicationObject.add(m1);
+		medicationSaves.stream().forEach(x -> System.out.println(x));
+
+		// medicationObject.add(m1);
 		System.out.println(medicationObject);
 		System.out.println();
 		medicationObject.stream().forEach(a -> System.out.println(a));
 		List<Medication> medication = medicationRepo.findAllMedication();
-	    medicationService.addMedication(medicationSaves);
-		//return new ResponseEntity<List<Medication>>(medication, HttpStatus.OK);
+		medicationService.addMedication(medicationSaves);
+		// return new ResponseEntity<List<Medication>>(medication, HttpStatus.OK);
 		return "save";
 	}
-	
+
 	@GetMapping("physician/getAddedMedication")
-	public ResponseEntity<List<MedicationSave>> getAddedMedication(){
+	public ResponseEntity<List<MedicationSave>> getAddedMedication() {
 		List<MedicationSave> medicationSave = medicationService.gettingAddedMedication();
-		
+
 		return new ResponseEntity<List<MedicationSave>>(medicationSave, HttpStatus.OK);
+	}
+
+	@GetMapping(value = "/medications/{pid}/{date}", produces = MediaType.APPLICATION_PDF_VALUE)
+	public ResponseEntity<InputStreamResource> customersReport(@PathVariable String pid,
+			@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) throws IOException {
+		List<MedicationSave> medications = (List<MedicationSave>) medicationSaveRepo.findByPidAndDate(pid, date);
+
+		System.out.println(medications);
+
+		ByteArrayInputStream bis = PDFGenerator.medicationPDFReport(medications);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Disposition", "inline; filename=medications.pdf");
+
+		return ResponseEntity.ok().headers(headers).contentType(MediaType.APPLICATION_PDF)
+				.body(new InputStreamResource(bis));
 	}
 }
--- UserAndAdminModule/src/main/java/com/citiustech/impact/pms/repository/MedicationSaveRepository.java
+++ UserAndAdminModule/src/main/java/com/citiustech/impact/pms/repository/MedicationSaveRepository.java
@@ -1,5 +1,8 @@
 package com.citiustech.impact.pms.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
@@ -9,5 +12,5 @@ import com.citiustech.impact.pms.model.MedicationSave;
 @Repository
 @EnableJpaRepositories
 public interface MedicationSaveRepository extends JpaRepository<MedicationSave, String> {
-
+	List<MedicationSave> findByPidAndDate(String pid, LocalDate date);
 }
